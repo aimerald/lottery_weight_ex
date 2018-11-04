fae599d5b2df59409e34ba0d1b38c1f6315efbc3
UG <suzaku622@gmail.com>
2018-11-04 20:55:54 +0900

    add metadata

--- mix.exs
+++ mix.exs
@@ -1,19 +1,31 @@
 defmodule WeightLottery.MixProject do
   use Mix.Project
 
+	@description """
+	  Elixir library for weighted drawing
+  """
+
   def project do
     [
       app: :weight_lottery,
       version: "0.1.0",
       elixir: "~> 1.6",
-      start_permanent: Mix.env() == :prod,
-      deps: deps()
+			name: "weight_lottery",
+			description: @description,
+			build_embedded: Mix.env == :prod,
+			start_permanent: Mix.env == :prod,
+      deps: deps(),
+			package: [
+				maintainers: ["aimerald"],
+				licenses: ["MIT"],
+				links: %{ "Github" => "https://github.com/aimerald/lottery_weight_ex" }
+			]
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
-    [
+		[
       extra_applications: [:logger]
     ]
   end
